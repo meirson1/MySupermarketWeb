6f3150b752a5519c8760488526249392b715e75b
meirson1 <noam220599@gmail.com>
2022-06-20 17:02:10 +0300

    Adding routes to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import HomePage from "./pages/HomePage";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="Container">
+      <Header/>
+      <HomePage />  
       <Login />
       <Register/>
+      <Footer/>
     </div>
   );
 }
