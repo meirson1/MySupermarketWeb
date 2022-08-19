a2019b7144229bffcdee1a1c6f461e98666b29c6
snir27 <snir.shelef@gmail.com>
2022-08-19 15:38:45 +0300

    Create pages + Home /  Cart / Product jsx files

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useEffect } from "react";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import { useEffect } from "react";
 
-export default function HomePage() {
-  const [clients,setClients] = useState([]);
-  useEffect(()=>{
-    axios.post("http://localhost:8080/db").then(res=>setClients(res.data));
-  },[])
+// export default function HomePage() {
+//   const [clients,setClients] = useState([]);
+//   useEffect(()=>{
+//     axios.post("http://localhost:8080/db").then(res=>setClients(res.data));
+//   },[])
   
-  return <div>
-    <h1>HomePage</h1>
-    {clients.map(client=>{
-      <p key={client.id}>{client.name}</p>
-    })}
-  </div>;
+//   return <div>
+//     <h1>HomePage</h1>
+//     {clients.map(client=>{
+//       <p key={client.id}>{client.name}</p>
+//     })}
+//   </div>;
+// }
+
+import "../styles/HomePage.css";
+import Product from "../components/Product";
+
+const HomePage = () => {
+  return <div className="homepage">
+      <div className="homepage__products">
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+      </div>
+ </div>
 }
+
+export default HomePage;
