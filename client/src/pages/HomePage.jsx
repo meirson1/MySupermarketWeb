2c8a4c6c6a851587374bd15b3d2992d1342c0852
snir27 <snir.shelef@gmail.com>
2022-08-20 15:26:31 +0300

    Connect HomePage to products from MongoDB

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -17,20 +17,42 @@
 // }
 
 import "../styles/HomePage.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// Components
 import Product from "../components/Product";
+// Actions
+import {getProducts as listProducts, } from "../redux/actions/productActions"
+
 
 const HomePage = () => {
-  return <div className="homepage">
+
+    const dispatch = useDispatch();
+    const getProducts = useSelector((state) => state.getProducts);
+    const { products, loading, error } = getProducts; 
+
+    useEffect(() =>{
+        dispatch(listProducts());
+    }, [dispatch]);
+
+  return ( 
+  <div className="homepage">
       <div className="homepage__products">
-          <Product/>
-          <Product/>
-          <Product/>
-          <Product/>
-          <Product/>
-          <Product/>
-          <Product/>
+        {loading ? (<h2>Loading...</h2>
+        ) : error ? (
+        <h2>{error}</h2> 
+        ) : products.map((product) => <Product 
+            key={product._id}
+            productId={product._id} 
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            rating={product.rating}
+            imageUrl={product.imageUrl}/>
+        )}
       </div>
  </div>
-}
+  ); 
+};
 
 export default HomePage;
