c524bf142631c33659d2785748d16bd17ea46239
snir27 <snir.shelef@gmail.com>
2022-08-20 15:25:31 +0300

    Connect Product to MongoDB

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,21 +1,20 @@
 import "../styles/Product.css";
 import { Link } from "react-router-dom";
-const Product = () => {
+
+const Product = ({imageUrl, name, price, description, rating, productId }) => {
   return (
     <div className="product">
-    <img  src="https://shop.wegmans.com/cdn-cgi/image/f=auto,q=80,dpr=1,h=200,w=200/https://d2d8wwwkmhfcva.cloudfront.net/800x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_075f3bee-baf9-4309-87a6-7f8331c01a00.jpg" 
-    alt="product name"/>
+    <img  src={imageUrl} alt={name}/>
     <div className="product__info">
-        <p className="info__name">Avocado</p>
+        <p className="info__name">{name}</p>
         <p className="info__description">
-        Avocado ready for eat (each)
-        </p>
+        {description.substring(0,100)}...</p>
         <p className="info__rating">
-        3 stars
+        {rating} stars
         </p>
-        <p className="info__price">$2.00</p>
-        <Link to={`/product/${1111}`} className="info__button">
-            Add To Cart
+        <p className="info__price">${price.toFixed(2)}</p>
+        <Link to={`/product/${productId}`} className="info__button">
+            View
         </Link>
     </div>  
     </div>
