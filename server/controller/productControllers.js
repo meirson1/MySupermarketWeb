1463d5f92b5b673b3fbb57ee42f395cf4e585cf4
snir27 <snir.shelef@gmail.com>
2022-08-20 15:43:44 +0300

    Remove log and update logs

--- server/controller/productControllers.js
+++ server/controller/productControllers.js
@@ -1,41 +1,39 @@
-const res = require("express/lib/response");
+const express = require("express");
 const Product = require("../models/Product");
 
-const getAllProducts = async (req,res) => {
-    try {
-        const products = await Product.find({});
-        if(products) {
-            res.status(200).send(products);
-            log.info('Products sent')
-        } else {
-            res.status(404).send({code: 404, message:`There is an error with your products`});
-        }
-    } catch (error) {
-        log.error(error);
-        res.status(500).send({message: error.message});
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}).sort({ name: "asc" });
+    if (products) {
+      res.status(200).send(products);
+      console.log("Products sent", products);
+    } else {
+      res
+        .status(404)
+        .send({ code: 404, message: `There is an error with your products` });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
 };
 
-const getProductById = async (req,res) => {
-    const {id} = req.params.id;
-
-    try {
-        const product = await Product.findById(id);
-        if(product) {
-            res.status(200).send(product);
-            log.info('Product sent');
-        }
-        else {
-            log.info({ id }, 'Product does not exist');
-            res.status(404).send({code: 404, message:`Product with id ${id} does not exist`});
-        }
-    } catch (error) {
-        log.error(error);
-        res.status(500).send({message: error.message});  
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.status(200).send(product);
+    } else {
+      console.info({ id }, "Product does not exist");
+      res.status(500).send({ message: error.message });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: error.message });
+  }
 };
 
 module.exports = {
-    getAllProducts,
-    getProductById,
+  getAllProducts,
+  getProductById,
 };
