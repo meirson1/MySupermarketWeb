82dfee3b7ed68ae3a894c99cb4c358da0f72e0cb
snir27 <snir.shelef@gmail.com>
2022-08-22 01:41:19 +0300

    Save cart to carts collection on MongoDB

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,5 +1,6 @@
 import * as actionTypes from "../constants/cartCons";
 import axios from "axios";
+import qs from "qs";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`http://localhost:8080/api/products/${id}`);
@@ -25,3 +26,24 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   });
   localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
+
+export const sendCartDataToDB =
+  (client, cartItems, totalPrice) => async (dispatch, getState) => {
+    dispatch({
+      type: actionTypes.CART_RESET,
+    });
+    localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+    const config = {
+      url: `http://localhost:8080/api/carts`,
+      method: "get",
+      params: {
+        products: cartItems,
+        totalPrice: totalPrice,
+        userId: client._id,
+      },
+      paramsSerializer: (params) => {
+        return qs.stringify(params);
+      },
+    };
+    const { data } = await axios(config);
+  };
