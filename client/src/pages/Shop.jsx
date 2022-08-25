7613e258a3f71b31a8873054e3b4cefa0621550a
snir27 <snir.shelef@gmail.com>
2022-08-25 04:29:41 +0300

    Create search bar departments selector

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,6 +1,7 @@
 import "../styles/HomePage.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Select from "react-select";
 // Components
 import Product from "../components/Product";
 // Actions
@@ -10,13 +11,81 @@ export default function Shop() {
   const dispatch = useDispatch();
   const getProducts = useSelector((state) => state.getProducts);
   const { products, loading, error } = getProducts;
-
+  const [query, setQuery] = useState("");
+  const [selected, setSelected] = useState({
+    value: "all",
+    label: "All Depertments",
+  });
   useEffect(() => {
-    dispatch(listProducts());
+    dispatch(listProducts(query, selected.value));
   }, [dispatch]);
+
+  const searchQuery = (q, d) => {
+    dispatch(listProducts(q, d));
+  };
+
+  const clearQuery = () => {
+    dispatch(listProducts("", "all"));
+  };
+
+  const handleChange = (searchEvent) => {
+    setQuery(searchEvent.target.value);
+    searchQuery(searchEvent.target.value, selected.value);
+  };
+
+  const handleClick = async () => {
+    setQuery("");
+    clearQuery();
+  };
+
+  const options = [
+    { value: "fruit", label: "Fruit" },
+    { value: "vegetable", label: "Vegetable" },
+    { value: "grocery", label: "Grocery" },
+    { value: "dairy", label: "Dairy" },
+    { value: "meat", label: "Meat" },
+    { value: "wine&beer", label: "Wine & Beer" },
+    { value: "bakery", label: "Bakery" },
+    { value: "all", label: "All Depertments" },
+  ].sort((a, b) => a.value.localeCompare(b.value));
 
   return (
     <div className="homepage">
+      <div>
+        <ul>
+          <input
+            className="homepage__search"
+            placeholder=" Search By Name..."
+            onChange={handleChange}
+            value={query}
+          />
+          <button
+            defaultValue
+            className="homepage__clearbutton"
+            disabled={query.length === 0 && selected.value === "all"}
+            onClick={() => {
+              setSelected({ value: "all", label: "All Depertments" });
+              handleClick();
+            }}
+          >
+            Clear
+          </button>
+          <div>
+            <Select
+              className="homepage__select"
+              value={selected}
+              options={options}
+              onChange={setSelected}
+            >
+              <option>Select</option>
+              {options &&
+                options.map((d, index) => {
+                  return <option key={index}>{d.value}</option>;
+                })}
+            </Select>
+          </div>
+        </ul>
+      </div>
       <div className="homepage__products">
         {loading ? (
           <h2>Loading...</h2>
