3daea9ff48918a3d58fede0d7bc9f10ab061ccb2
Noam Meir <93981788+meirson1@users.noreply.github.com>
2022-08-25 20:09:00 +0300

    Dafna's CSS Fix

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import "../styles/HomePage.css"
+import "../styles/HomePage.css";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -24,27 +24,27 @@ function HomePage() {
 
   return (
     <>
-    <div className="Home__warpper">
-      <div className="heading">
+      <div className="Home__warpper">
+        <div className="heading">
+          <br />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "right",
+            }}
+          >
+            <h1>Welcome {client && client.name}</h1>
+          </div>
+        </div>
         <br />
-        <div 
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "right",
-          }}
-        >
-          <h1>Welcome {client && client.name}</h1>
+        <Video embedId="LC_qM1P6nG4" />
+        <br />
+        <div style={{ display: "contents" }}>
+          <Maps className="Home__map" />
+          <Weather className="Home__weather" />
         </div>
-        </div>
-      <br />
-      <Video embedId="LC_qM1P6nG4" />
-      <br />
-      <div style={{ display: "flex", flexdirection: "row" }}>
-        <Maps className="Home__map" />
-        <Weather className="Home__weather" />
       </div>
-       </div>
     </>
   );
 }
--- client/src/pages/Maps.jsx
+++ client/src/pages/Maps.jsx
@@ -1,38 +1,55 @@
-import { LoadScript } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { GoogleMap, MarkerF, InfoWindow } from "@react-google-maps/api";
+import { getLocations as listLocations } from "../redux/actions/locationActions";
 
-// Componnents
-import Map from "../components/Map";
+function Map() {
+  const dispatch = useDispatch();
+  const getLocations = useSelector((state) => state.getLocations);
+  const { locations } = getLocations;
 
-class LoadScriptOnlyIfNeeded extends LoadScript {
-  componentDidMount() {
-    const cleaningUp = true;
-    const isBrowser = typeof document !== "undefined"; // require('@react-google-maps/api/src/utils/isbrowser')
-    const isAlreadyLoaded =
-      window.google &&
-      window.google.maps &&
-      document.querySelector("body.first-hit-completed"); // AJAX page loading system is adding this class the first time the app is loaded
-    if (!isAlreadyLoaded && isBrowser) {
-      // @ts-ignore
-      if (window.google && !cleaningUp) {
-        console.error("google api is already presented");
-        return;
-      }
+  useEffect(() => {
+    dispatch(listLocations());
+  }, [dispatch]);
 
-      this.isCleaningUp().then(this.injectScript);
-    }
+  const [selected, setSelected] = useState({});
 
-    if (isAlreadyLoaded) {
-      this.setState({ loaded: true });
-    }
-  }
+  const onSelect = (item) => {
+    setSelected(item);
+  };
+
+  const mapStyles = {
+    // height: "40vh",
+    // width: "40%",
+    gridArea: "map",
+  };
+
+  const defaultCenter = {
+    lat: 32.07018929655174,
+    lng: 34.794164715631155,
+  };
+
+  return (
+    <GoogleMap mapContainerStyle={mapStyles} zoom={15} center={defaultCenter}>
+      {locations.map((item) => (
+        <MarkerF
+          key={item.name}
+          position={{ lat: item.lat, lng: item.lng }}
+          onClick={() => onSelect(item)}
+          icon="https://img.icons8.com/color/48/000000/shopping-cart-loaded.png"
+        ></MarkerF>
+      ))}
+      {selected.lat && selected.lng && (
+        <InfoWindow
+          position={{ lat: selected.lat, lng: selected.lng }}
+          clickable={true}
+          onCloseClick={() => setSelected({})}
+        >
+          <p>{selected.name}</p>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
 
-const Maps = () => {
-  return (
-    <LoadScriptOnlyIfNeeded googleMapsApiKey="AIzaSyCbS0WcrG3esjA5jBtqs_N1Ll3bGkm4Z5k">
-      <Map />
-    </LoadScriptOnlyIfNeeded>
-  );
-};
-
-export default Maps;
+export default Map;
